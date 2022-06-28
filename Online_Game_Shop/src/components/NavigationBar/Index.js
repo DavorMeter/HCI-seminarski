f86f05adea5648739e74c0766b365e287f272d9d
DadoMeter <dmeter00@fesb.hr>
2022-06-28 14:42:14 +0200

    refaktorirano da korisim map()

--- Online_Game_Shop/src/components/NavigationBar/Index.js
+++ Online_Game_Shop/src/components/NavigationBar/Index.js
@@ -5,15 +5,19 @@ import {
     active
 } from './layout.module.css'
 
+const navTabs = ['HOME','STORE','ABOUT','SUPPORT','LOGIN']
 const NavigationBar = () =>
-<section>
-    <nav className={navigationBar}>
-        <li className={active}>HOME</li>
-        <li>STORE</li>
-        <li>ABOUT</li>
-        <li>SUPPORT</li>
-        <li>LOGIN</li>
-    </nav>
-</section>
+<nav className={navigationBar}>
+    {navTabs.map(tab => {
+      if (tab === 'HOME')
+      return (
+        <li className={active}>
+          {tab}
+        </li>)
+      else
+        return <li>{tab}</li>
+      }
+    )}
+</nav>
 
 export default NavigationBar
