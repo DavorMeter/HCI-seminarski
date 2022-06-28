ed7c72e5e96f7aa292243f9f505c2c02c141741c
DadoMeter <dmeter00@fesb.hr>
2022-06-28 11:17:53 +0200

    Dodan navbar i global css

---
+++ Online_Game_Shop/src/pages/store.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+const StorePage = () => {
+  return (
+    <Layout pageTitle="Store page">
+      <p>StorePage</p>
+    </Layout>
+  )
+}
+
+export default StorePage
---
+++ Online_Game_Shop/src/pages/support.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+const SupportPage = () => {
+  return (
+    <Layout pageTitle="Support page">
+      <p>SupportPage</p>
+    </Layout>
+  )
+}
+
+export default SupportPage
